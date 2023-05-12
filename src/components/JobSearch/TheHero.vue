7ede37144f763415ff50ec7dc4dbec4f83d124ff
IndieVisionDev <indievision.dev@protonmail.com>
2023-05-12 18:18:41 -0500

    section 24

--- src/components/JobSearch/TheHero.vue
+++ src/components/JobSearch/TheHero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex h-screen flex-col pt-10 pb-20">
+  <main class="flex flex-col h-screen">
+    <section class="pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1"></div>
 
@@ -9,9 +9,9 @@
           <job-search-form />
         </div>
 
-        <div class="col-span-5 col-start-7 self-center justify-self-center">
+        <div class="self-center col-span-5 col-start-7 justify-self-center">
           <img
-            class="h-80 w-80 object-contain"
+            class="object-contain h-80 w-80"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"
           />
         </div>
@@ -19,18 +19,47 @@
         <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
+
+    <spot-light class="flex flex-row justify-center pb-16">
+      <template #default="{ img, title, description }">
+        <router-link
+          to="/jobs/results"
+          class="flex flex-col mx-5 border rounded-lg h-96 w-72 bg-brand-gray-2"
+        >
+          <img :src="img" class="object-contain" />
+
+          <div class="h-48 px-6 py-4 mt-3">
+            <h3 class="text-lg font-medium">
+              {{ title }}
+            </h3>
+
+            <p class="mt-3 text-sm">
+              {{ description }}
+            </p>
+          </div>
+
+          <router-link
+            to="/jobs/results"
+            class="px-6 pb-4 text-sm text-brand-blue-1"
+            >See jobs</router-link
+          >
+        </router-link>
+      </template>
+    </spot-light>
   </main>
 </template>
 
 <script>
 import TheHeadline from "@/components/JobSearch/TheHeadline.vue";
 import JobSearchForm from "@/components/JobSearch/JobSearchForm.vue";
+import SpotLight from "@/components/JobSearch/SpotLight.vue";
 
 export default {
   name: "TheHero",
   components: {
     TheHeadline,
     JobSearchForm,
+    SpotLight,
   },
 };
 </script>
